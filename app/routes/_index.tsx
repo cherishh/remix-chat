bed1718041dd953b8eb738728ec1c4940941c84b
wangzhongxi <wangzhongxi@meituan.com>
2024-02-22 20:06:58 +0800

    feat: add type

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,18 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
+import { json, useLoaderData } from '@remix-run/react';
+import supabase from 'utils/supabase';
+
+export const loader = async () => {
+  const { data, error } = await supabase.from('messages').select('*');
+  if (error) throw error;
+  return { data };
+};
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+  return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }];
 };
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>hello chatter</h1>
-    </div>
-  );
+  const { data } = useLoaderData();
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
 }
